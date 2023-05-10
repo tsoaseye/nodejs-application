0a1b5d42e30ae8c8de592504d352344cc0f39894
kelvin <127989292+floatt1@users.noreply.github.com>
2023-05-10 02:35:08 +0000

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.30.64:9000/',
+  serverUrl: 'http://23.22.244.182:8000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsJomacsit',
-	    'sonar.login': '7ac55fa453edeb261a8eb887aacc2283393a93da',
+	    'sonar.login': '73e279fcb46164fb832e53de6d23af5bc2f783f9',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
